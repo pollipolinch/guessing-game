8caa14639ed10209b58f3bab41a7ac26744921a5
Полина <a1@MacBook-Air-Polina.local>
2023-02-23 03:23:07 +0500

    feat: done game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+      }
+  
+      guess() {
+        this.number = Math.round((this.max+this.min) / 2)
+        return this.number
+      }
+  
+      lower() {
+        this.max = this.number
+      }
+  
+      greater() {
+        this.min = this.number
+      }
     }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
 
 module.exports = GuessingGame;
